b0ed3cc2cd8385a2cceefe095c459ce453f6b37c
Prowl <prowlcorp@gmail.com>
2020-09-30 22:28:18 -0500

    Update abilities.js

--- data/abilities.js
+++ data/abilities.js
@@ -2014,10 +2014,9 @@ let BattleAbilities = {
 		onAnyModifyMove(move, pokemon) {
 			if (move.type === 'Poison' && !pokemon.hasAbility('holytoxin')) {
 				move.onTryMove = function(pokemon, target, move) {
-//					this.add('cant', 'ability: Holy Toxin');
-					this.add('cant', this.effectData.target, 'ability: Holy Toxin', move, '[of] ' + target);
+					this.add('-fail', pokemon, '[from] ability: Holy Toxin');
 					this.attrLastMove('[still]');
-					return false;
+					return null;
 				}
 			}
 		},
