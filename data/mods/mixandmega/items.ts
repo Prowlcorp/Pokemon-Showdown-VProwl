81578724edd13c2bedfcf41afc9e048bcf03b2be
Prowl <prowlcorp@gmail.com>
2020-11-01 10:26:07 -0600

    Overhaul 1

---
+++ data/mods/mixandmega/items.ts
@@ -0,0 +1,143 @@
+export const Items: {[k: string]: ModdedItemData} = {
+	abomasite: {
+		inherit: true,
+	},
+	absolite: {
+		inherit: true,
+	},
+	aerodactylite: {
+		inherit: true,
+	},
+	aggronite: {
+		inherit: true,
+	},
+	alakazite: {
+		inherit: true,
+	},
+	altarianite: {
+		inherit: true,
+	},
+	ampharosite: {
+		inherit: true,
+	},
+	audinite: {
+		inherit: true,
+	},
+	banettite: {
+		inherit: true,
+	},
+	beedrillite: {
+		inherit: true,
+	},
+	blastoisinite: {
+		inherit: true,
+	},
+	blazikenite: {
+		inherit: true,
+	},
+	cameruptite: {
+		inherit: true,
+	},
+	charizarditex: {
+		inherit: true,
+	},
+	charizarditey: {
+		inherit: true,
+	},
+	diancite: {
+		inherit: true,
+	},
+	galladite: {
+		inherit: true,
+	},
+	garchompite: {
+		inherit: true,
+	},
+	gardevoirite: {
+		inherit: true,
+	},
+	gengarite: {
+		inherit: true,
+	},
+	glalitite: {
+		inherit: true,
+	},
+	gyaradosite: {
+		inherit: true,
+	},
+	heracronite: {
+		inherit: true,
+	},
+	houndoominite: {
+		inherit: true,
+	},
+	kangaskhanite: {
+		inherit: true,
+	},
+	latiasite: {
+		inherit: true,
+	},
+	latiosite: {
+		inherit: true,
+	},
+	lopunnite: {
+		inherit: true,
+	},
+	lucarionite: {
+		inherit: true,
+	},
+	manectite: {
+		inherit: true,
+	},
+	mawilite: {
+		inherit: true,
+	},
+	medichamite: {
+		inherit: true,
+	},
+	metagrossite: {
+		inherit: true,
+	},
+	mewtwonitex: {
+		inherit: true,
+	},
+	mewtwonitey: {
+		inherit: true,
+	},
+	pidgeotite: {
+		inherit: true,
+	},
+	pinsirite: {
+		inherit: true,
+	},
+	sablenite: {
+		inherit: true,
+	},
+	salamencite: {
+		inherit: true,
+	},
+	sceptilite: {
+		inherit: true,
+	},
+	scizorite: {
+		inherit: true,
+	},
+	sharpedonite: {
+		inherit: true,
+	},
+	slowbronite: {
+		inherit: true,
+	},
+	steelixite: {
+		inherit: true,
+	},
+	swampertite: {
+		inherit: true,
+	},
+	tyranitarite: {
+		inherit: true,
+	},
+	venusaurite: {
+		inherit: true,
+	},
+};
