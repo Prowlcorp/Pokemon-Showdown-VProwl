176426b68f4011f5d97dca372800c707eddeff37
Prowl <Prowlcorp@gmail.com>
2021-03-15 04:17:10 -0500

    Update scripts.ts

--- data/scripts.ts
+++ data/scripts.ts
@@ -1243,7 +1243,7 @@ export const Scripts: BattleScriptsData = {
 			}
 		}
 
-		pokemon.formeChange(speciesid, pokemon.getItem(), true);
+		pokemon.formeChange(speciesid);
 
 		this.runEvent('AfterFormChange', pokemon);
 		return true;
